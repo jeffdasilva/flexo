d0c62b3b0cd85b6da9ad144850afbb86097cf6c3
jeffdasilva <jeffdasilva@users.noreply.github.com>
2024-03-29 23:41:49 +0000

    clean up git targets to remove duplication

--- plugins/git/flexo.mk
+++ plugins/git/flexo.mk
@@ -2,18 +2,19 @@
 git.exe ?= git
 git.cmd ?= $(git.exe) $(if $(git.PATH),-C $(git.PATH))
 
-.PHONY: git.status
-git.status:
-	$(git.cmd) status
+.PHONY: sync
+sync: git.pull
+
+.PHONY: diff
+diff: git.diff
+
+git.ALIAS_COMMANDS += status pull push commit diff
+git.ALIAS_TARGETS += $(patsubst %,git.%, $(git.ALIAS_COMMANDS))
+
+.PHONY: $(git.ALIAS_TARGETS)
+$(git.ALIAS_TARGETS): git.%:
+	$(git.cmd) $*
 
 .PHONY: git.stage
 git.stage:
 	$(git.cmd) add .
-
-.PHONY: git.commit
-git.commit:
-	$(git.cmd) commit
-
-.PHONY: git.push
-git.push:
-	$(git.cmd) push
