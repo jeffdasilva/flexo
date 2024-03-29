aee55d3a24dfa437e141cb8995aef514664581d8
jeffdasilva <jeffdasilva@users.noreply.github.com>
2024-03-29 05:15:30 +0000

    add some early scaffolding api mostly for plugin discovery

--- flexo.mk
+++ flexo.mk
@@ -6,11 +6,16 @@
 # Code licensed under MIT 2024 (c) Jeff DaSilva
 ###############################################################################
 
-FLEXO.ROOT_MAKEFILE := $(abspath $(lastword $(MAKEFILE_LIST)))
+ifneq ($(FLEXO.ROOT_DIR),)
+$(error ERROR: FLEXO.ROOT_DIR already defined. This makefile should be included only once)
+endif
+
+FLEXO.ROOT_MAKEFILE := $(abspath $(subst \,/,$(lastword $(MAKEFILE_LIST))))
 FLEXO.ROOT_DIR := $(patsubst %/,%,$(dir $(FLEXO.ROOT_MAKEFILE)))
-FLEXO.PLUGINS_DIR := $(FLEXO.ROOT_DIR)/plugins
+FLEXO.PLUGINS_DIR += $(FLEXO.ROOT_DIR)/plugins
 
 FLEXO.MAKECMDGOALS := $(filter flexo.%,$(MAKECMDGOALS))
+FLEXO.VARIABLES_INIT := $(.VARIABLES)
 
 ###############################################################################
 # Stuff that gnu make should enable by default (in my opinion) 
@@ -23,20 +28,73 @@ SHELL := /bin/bash
 override empty :=
 override SPACE := $(empty) $(empty)
 COMMA := ,
+
+define tolower
+$(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f,$(subst G,g,$(subst H,h,$(subst I,i,$(subst J,j,$(subst K,k,$(subst L,l,$(subst M,m,$(subst N,n,$(subst O,o,$(subst P,p,$(subst Q,q,$(subst R,r,$(subst S,s,$(subst T,t,$(subst U,u,$(subst V,v,$(subst W,w,$(subst X,x,$(subst Y,y,$(subst Z,z,$1))))))))))))))))))))))))))
+endef
+
+define toupper
+$(subst a,A,$(subst b,B,$(subst c,C,$(subst d,D,$(subst e,E,$(subst f,F,$(subst g,G,$(subst h,H,$(subst i,I,$(subst j,J,$(subst k,K,$(subst l,L,$(subst m,M,$(subst n,N,$(subst o,O,$(subst p,P,$(subst q,Q,$(subst r,R,$(subst s,S,$(subst t,T,$(subst u,U,$(subst v,V,$(subst w,W,$(subst x,X,$(subst y,Y,$(subst z,Z,$1))))))))))))))))))))))))))
+endef
+
+FLEXO.VERBOSE ?= $(VERBOSE)
+QUIET = $(if $(call flexo.true,$(FLEXO.VERBOSE)),,@)
+
 ###############################################################################
 
-ifeq ($(FLEXO.DEBUG),1)
-define flexo.debug
-$(info FLEXO_DEBUG: $(1))
+define flexo.true
+$(if $(filter-out 0 n no f false disable,$(call tolower,$1)),T,)
 endef
-endif
+
+define flexo.false
+$(if $(call flexo.true,$1),,T)
+endef
+
+define flexo.debug
+$(if $(call flexo.true,$(FLEXO.DEBUG)),$(info FLEXO_DEBUG: $(1)))
+endef
+
+define flexo.warning
+$(warning FLEXO_WARNING: $(1))
+endef
+
+define flexo.error
+$(error FLEXO_ERROR: $(1))
+endef
+
 
 ifneq ($(filter flexo.update,$(FLEXO.MAKECMDGOALS)),)
 .PHONY: flexo.update
 flexo.update:
-	@echo "Updating flexo..."
-	@cd $(FLEXO.ROOT_DIR) && git pull
+	@echo "Updating Flexo..."
+	$(QUIET)git -C $(FLEXO.ROOT_DIR) pull
 endif
 
-FLEXO.PLUGINS := $(patsubst $(FLEXO.PLUGINS_DIR)/%/inc.mk,%,$(wildcard $(FLEXO.PLUGINS_DIR)/*/inc.mk))
+define flexo.discover
+$(strip \
+$(if $1,,$(call flexo.error,ARG1 [plugin_dir] not specified for function $0))
+$(foreach plugin_dir,$1,\
+	$(eval plugin_dir_abs = $(abspath $(plugin_dir)))
+
+	$(foreach plugin_mk,$(wildcard $(plugin_dir_abs)/*/flexo.mk),\
+      $(eval plugin_name = $(patsubst $(plugin_dir_abs)/%/flexo.mk,%,$(plugin_mk)))
+	  
+	  $(if $(filter $(plugin_name),$(FLEXO.PLUGINS)),\
+	     $(call flexo.warning,Plugin $(plugin_name) already loaded. Skipping...),\
+	     $(call flexo.debug,Discover Plugin: $(plugin_name)))
+
+	  $(eval FLEXO.PLUGINS += $(plugin_name))
+	  $(eval $(plugin_name).flexo.mk := $(abspath $(plugin_mk)))
+   )
+))
+endef
+
+ifneq ($(FLEXO.PLUGINS),)
+$(call flexo.error,FLEXO.PLUGINS already set [$(FLEXO.PLUGINS)]. This is not expected)
+endif
+FLEXO.PLUGINS := 
+$(call flexo.discover,$(FLEXO.PLUGINS_DIR))
+
+#$(patsubst $(FLEXO.PLUGINS_DIR)/%/flexo.mk,%,$(wildcard $(FLEXO.PLUGINS_DIR)/*/flexo.mk))
 $(call flexo.debug,Available Plugins: $(FLEXO.PLUGINS))
+
