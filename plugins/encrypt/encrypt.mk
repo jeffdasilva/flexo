aeace00b6640576557754959f7d63ac29d058a03
Jeff DaSilva <jeff.dasilva@gmail.com>
2024-10-13 17:13:35 -0700

    Add feature to encrypt files using openssl

---
+++ plugins/encrypt/encrypt.mk
@@ -0,0 +1,45 @@
+
+
+#encrypt.files = 
+encrypt.files_enc = $(addsuffix .enc,$(encrypt.files))
+
+.PHONY: decrypt
+decrypt: $(encrypt.files)
+
+.PHONY: encrypt
+encrypt: $(encrypt.files_enc)
+
+ifeq ($(filter decrypt,$(MAKECMDGOALS)),decrypt)
+
+ifeq ($(FORCE),YES)
+.PHONY: $(encrypt.files)
+endif
+
+ $(encrypt.files): %: %.enc
+ifeq ($(FORCE),YES)
+	$(if $(wildcard $@),@mv $@ $@.bak)
+else
+	$(if $(wildcard $@),$(error ERROR: $@ already exists. Refusing to overwrite without FORCE=YES))
+endif
+	openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 100000 -salt -in $< -out $@
+
+ifeq ($(FORCE),YES)
+	@if [ -f $@.bak ]; then \
+		echo "Diff of $@.bak and $@"; \
+		diff -u $@.bak $@; \
+		$(RM) $@.bak; \
+	fi
+endif
+
+else ifeq ($(filter encrypt,$(MAKECMDGOALS)),encrypt)
+
+$(encrypt.files_enc): %.enc: %
+	openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -in $< -out $@
+
+endif
+
+.PHONY: encrypt.install
+encrypt.install:
+	$(if $(or $(filter $@,$(MAKECMDGOALS)),$(call do_not_have_exe,openssl)),sudo apt -y install openssl)
+
+install: encrypt.install
