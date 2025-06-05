643a54eecb159a71f13772f3cfa9d2013db787bb
Jeff DaSilva <jeff.dasilva@gmail.com>
2025-06-04 22:46:52 -0700

    don't install chrome dep packages because it seems to break act/docker build flow

--- plugins/selenium/selenium.mk
+++ plugins/selenium/selenium.mk
@@ -42,14 +42,14 @@ ifeq ($(selenium.chrome.installed),)
 	rm -f chrome-linux64.zip*
 	wget $(call selenium.chrome.latest_url,chrome)
 
-	@echo "[$@] Install Chrome dependencies..."
-	sudo apt install -y ca-certificates fonts-liberation \
-    	libappindicator3-1 libasound2t64 libasound2-plugins libatk-bridge2.0-0 libatk1.0-0 libc6 \
-    	libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 \
-    	libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 \
-    	libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 \
-    	libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
-    	libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
+#	@echo "[$@] Install Chrome dependencies..."
+#	sudo apt install -y ca-certificates fonts-liberation \
+#    	libappindicator3-1 libasound2t64 libasound2-plugins libatk-bridge2.0-0 libatk1.0-0 libc6 \
+#    	libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 \
+#    	libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 \
+#    	libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 \
+#    	libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
+#    	libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
 
 	mkdir -p bin
 	unzip -o chrome-linux64.zip -d bin
